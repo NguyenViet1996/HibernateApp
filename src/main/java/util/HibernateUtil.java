6765e4e4813faf87ad03c07add41190261ce1294
ANH_VIET <Viet>
2022-06-27 07:50:02 +0700

    "customize - step 1 :work on entity Employee"
    This reverts commit a9e35235d8d17e89e5a5973265659f2f4e01fbf5.

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -1,94 +1,44 @@
-//package util;
-//
-//import org.hibernate.SessionFactory;
-//import org.hibernate.boot.Metadata;
-//import org.hibernate.boot.MetadataSources;
-//import org.hibernate.boot.registry.StandardServiceRegistry;
-//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-//
-//public class HibernateUtil {
-//    private static StandardServiceRegistry registry;
-//    private static SessionFactory sessionFactory;
-//
-//    public static SessionFactory getSessionFactory() {
-//        if (sessionFactory == null) {
-//            try {
-//                // Create registry
-//                registry = new StandardServiceRegistryBuilder().configure().build();
-//
-//                // Create MetadataSources
-//                MetadataSources sources = new MetadataSources(registry);
-//
-//                // Create Metadata
-//                Metadata metadata = sources.getMetadataBuilder().build();
-//
-//                // Create SessionFactory
-//                sessionFactory = metadata.getSessionFactoryBuilder().build();
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                if (registry != null) {
-//                    StandardServiceRegistryBuilder.destroy(registry);
-//                }
-//            }
-//        }
-//        return sessionFactory;
-//    }
-//
-//    public static void shutdown() {
-//        if (registry != null) {
-//            StandardServiceRegistryBuilder.destroy(registry);
-//        }
-//    }
-//
-//}
 package util;
 
-import java.util.Properties;
-
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
-
-import entity.Employee;
 
 public class HibernateUtil {
-    private static SessionFactory sessionFactory;
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
+		private static StandardServiceRegistry registry;
+		private static SessionFactory sessionFactory;
 
-                // Hibernate settings equivalent to hibernate.cfg.xml's properties
-                Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "org.hsqldb.jdbcDriver");
-                settings.put(Environment.URL, "jdbc:hsqldb:mem://localhost/testdb");
-                settings.put(Environment.USER, "SA");
-                settings.put(Environment.PASS, " ");
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.HSQLDialect");
+		public static SessionFactory getSessionFactory() {
+				if (sessionFactory == null) {
+						try {
+								// Create registry
+								registry = new StandardServiceRegistryBuilder().configure().build();
 
-                settings.put(Environment.SHOW_SQL, "true");
+								// Create MetadataSources
+								MetadataSources sources = new MetadataSources(registry);
 
-                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+								// Create Metadata
+								Metadata metadata = sources.getMetadataBuilder().build();
 
-                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+								// Create SessionFactory
+								sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-                configuration.setProperties(settings);
+						} catch (Exception e) {
+								e.printStackTrace();
+								if (registry != null) {
+										StandardServiceRegistryBuilder.destroy(registry);
+								}
+						}
+				}
+				return sessionFactory;
+		}
 
-                configuration.addAnnotatedClass(Employee.class);
+		public static void shutdown() {
+				if (registry != null) {
+						StandardServiceRegistryBuilder.destroy(registry);
+				}
+		}
 
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
-
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return sessionFactory;
-    }
 }
-
-
