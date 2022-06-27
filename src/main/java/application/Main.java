a9e35235d8d17e89e5a5973265659f2f4e01fbf5
ANH_VIET <Viet>
2022-06-27 07:48:32 +0700

    sustomize - step 1 :work on entity Employee

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,92 +1,21 @@
 package application;
 
+import persistent.EmployeeManager;
 import entity.Employee;
-import entity.ManagerRoom;
-import entity.Boss;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import persistence.SessionRemove;
-import util.HibernateUtil;
-
-import javax.persistence.Query;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+         EmployeeManager employeeManager = new EmployeeManager();
+         Employee employee = new Employee("viet","0986091614",10,112);
+         employeeManager.saveEmployee(employee);
 
-        // create object of employee
-//        Employee employee1 = new Employee("Viet", "0986091614", 10, 15);
-//        Employee employee2 = new Employee("Hai", "0904423479", 5, 8);
-//        Employee employee3 = new Employee("Hai", "0904423479", 5, 8);
-//
-//
-//        // create object of ManagerRoom
-//        ManagerRoom managerRoom1 = new ManagerRoom("nam", "1111111", 12, 20);
-//        ManagerRoom managerRoom2 = new ManagerRoom("nam", "1111111", 12, 20);
-//
-//        // create objects of Boss
-//        Boss boss1 = new Boss("Nam", "7888888888", 8, 12.5);
-//        Boss boss2 = new Boss("Nam", "7888888888", 8, 12.5);
-//        Boss boss3 = new Boss("Nam", "7888888888", 8, 12.5);
-//        Boss boss4 = new Boss("Nam", "7888888888", 8, 12.5);
-//
-//        Transaction transaction = null;
-////        String stringDeleteEmployee = "DELETE FROM employee";
-//        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-//            // start a transaction
-//            transaction = session.beginTransaction();
-////            session.save(employee1);
-////            session.save(employee2);
-////            session.save(employee3);
-//            sessionRemove.removeEmployee(54);
-//            // commit transaction
-//            transaction.commit();
+         // insert into Employee table
+        employeeManager.insertEmployee();
 
-            // start a transaction
-//            transaction = session.beginTransaction();
-//            session.save(managerRoom1);
-//            session.save(managerRoom2);
-            // commit transaction
-//            transaction.commit();
-
-            // start a transaction
-//            transaction = session.beginTransaction();
-//            session.save(boss1);
-//            session.save(boss2);
-//            session.save(boss3);
-//            session.save(boss4);
-            // commit transaction
-//            transaction.commit();
-
-//        } catch (Exception e) {
-//            if (transaction != null) {
-//                transaction.rollback();
-//            }
-//            e.printStackTrace();
-//        }
-
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = null;
-        try {
-            String hql = "delete from Employee e where e.id=54";
-            transaction = session.beginTransaction();
-            Query query = session.createQuery(hql);
-//            query.setParameter("id",54);
-            int rowCount = query.executeUpdate();
-            transaction.commit();
-            if (rowCount == 0) {
-                System.out.println("No data fodun to delete");
-            } else {
-                System.out.println("Your record is deleted");
-            }
-        } catch (HibernateException e){
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
+         // update into Employee table
+        Employee employee1 = new Employee("tai","123456789",15,102);
+        employeeManager.updateEmployee(employee1);
         }
     }
 
--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -1,44 +1,94 @@
+//package util;
+//
+//import org.hibernate.SessionFactory;
+//import org.hibernate.boot.Metadata;
+//import org.hibernate.boot.MetadataSources;
+//import org.hibernate.boot.registry.StandardServiceRegistry;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//
+//public class HibernateUtil {
+//    private static StandardServiceRegistry registry;
+//    private static SessionFactory sessionFactory;
+//
+//    public static SessionFactory getSessionFactory() {
+//        if (sessionFactory == null) {
+//            try {
+//                // Create registry
+//                registry = new StandardServiceRegistryBuilder().configure().build();
+//
+//                // Create MetadataSources
+//                MetadataSources sources = new MetadataSources(registry);
+//
+//                // Create Metadata
+//                Metadata metadata = sources.getMetadataBuilder().build();
+//
+//                // Create SessionFactory
+//                sessionFactory = metadata.getSessionFactoryBuilder().build();
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                if (registry != null) {
+//                    StandardServiceRegistryBuilder.destroy(registry);
+//                }
+//            }
+//        }
+//        return sessionFactory;
+//    }
+//
+//    public static void shutdown() {
+//        if (registry != null) {
+//            StandardServiceRegistryBuilder.destroy(registry);
+//        }
+//    }
+//
+//}
 package util;
 
+import java.util.Properties;
+
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import entity.Employee;
 
 public class HibernateUtil {
-		private static StandardServiceRegistry registry;
-		private static SessionFactory sessionFactory;
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
 
-		public static SessionFactory getSessionFactory() {
-				if (sessionFactory == null) {
-						try {
-								// Create registry
-								registry = new StandardServiceRegistryBuilder().configure().build();
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "org.hsqldb.jdbcDriver");
+                settings.put(Environment.URL, "jdbc:hsqldb:mem://localhost/testdb");
+                settings.put(Environment.USER, "SA");
+                settings.put(Environment.PASS, " ");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.HSQLDialect");
 
-								// Create MetadataSources
-								MetadataSources sources = new MetadataSources(registry);
+                settings.put(Environment.SHOW_SQL, "true");
 
-								// Create Metadata
-								Metadata metadata = sources.getMetadataBuilder().build();
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-								// Create SessionFactory
-								sessionFactory = metadata.getSessionFactoryBuilder().build();
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
 
-						} catch (Exception e) {
-								e.printStackTrace();
-								if (registry != null) {
-										StandardServiceRegistryBuilder.destroy(registry);
-								}
-						}
-				}
-				return sessionFactory;
-		}
+                configuration.setProperties(settings);
 
-		public static void shutdown() {
-				if (registry != null) {
-						StandardServiceRegistryBuilder.destroy(registry);
-				}
-		}
+                configuration.addAnnotatedClass(Employee.class);
 
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
 }
+
+
